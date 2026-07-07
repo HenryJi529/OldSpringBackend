2d8cea36a23e5a9af7a96a6a27ec5473b4935b69
Henry Ji <jeep.jipu@gmail.com>
2026-07-07 11:15:18 +0800

    init

---
+++ backend/src/main/java/com/morningstar/old/infra/constant/RedisConstant.java
@@ -0,0 +1,13 @@
+package com.morningstar.old.infra.constant;
+
+public class RedisConstant {
+    /**
+     * 前缀后的分隔符
+     */
+    public static final String KEY_SEPARATOR = ":";
+
+    /**
+     * 认证相关前缀
+     */
+    public static final String AUTH_LOGIN = "auth:login"; // UserDetails
+}
---
+++ backend/src/main/java/com/morningstar/old/system/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package com.morningstar.old.system.service.impl;
+
+import com.morningstar.old.system.pojo.bo.LoginUser;
+import com.morningstar.old.system.pojo.po.User;
+import com.morningstar.old.system.properties.AuthProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final AuthProperties authProperties;
+
+    @Override
+    public UserDetails loadUserByUsername(String account) throws UsernameNotFoundException {
+        User user = authProperties.getUsers().stream().filter(item -> item.getAccount().equals(account)).collect(Collectors.toList()).get(0);
+        if (user == null) {
+            throw new UsernameNotFoundException("用户名不存在");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        List<String> roleTags = authProperties.getUserRoles().entrySet().stream().filter(entry -> entry.getKey().equals(user.getAccount())).collect(Collectors.toList()).get(0).getValue();
+        List<String> permissionTags = authProperties.getRolePermissions().entrySet().stream()
+                .filter(entry -> roleTags.contains(entry.getKey()))
+                .flatMap(entry -> entry.getValue().stream()).collect(Collectors.toList());
+
+        return new LoginUser(user, Stream.concat(roleTags.stream().map(item -> "ROLE_" + item), permissionTags.stream()).collect(Collectors.toSet()));
+    }
+}
